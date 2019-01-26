4253b63ad1edb50b0f22e146b65612b5b37345a2
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-01-26 01:37:22 -0800

    Updated Ammo

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -7,12 +7,19 @@ public class Ammo : MonoBehaviour
     private Transform spawner;
     private Rigidbody2D rb2d;
     public float speed = 10;
+    public GameObject ammoType;
+    Vector2 v;
     
     // Start is called before the first frame update
     void Start()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Transform>();
         rb2d = GetComponent<Rigidbody2D>();
+        if (spawner.localPosition.x != 0)
+            v = rb2d.velocity = new Vector2(speed * spawner.localPosition.x, 0);
+
+        if (spawner.localPosition.y != 0)
+            v = rb2d.velocity = new Vector2(0, speed * spawner.localPosition.y);
     }
 
     // Update is called once per frame
@@ -27,6 +34,27 @@ public class Ammo : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        Vector3 spawnLocation = col.gameObject.transform.position;
+
+        if (v.x > 0)
+        {
+            spawnLocation += new Vector3(-1.5f, 0, 0);
+        }
+        else if (v.x < 0)
+        {
+            spawnLocation += new Vector3(1.5f, 0, 0);
+        }
+        else if (v.y > 0)
+        {
+            spawnLocation += new Vector3(0, -1.5f, 0);
+        }
+        else if (v.y < 0)
+        {
+            spawnLocation += new Vector3(0, 1.5f, 0);
+        }
+
+        Instantiate(ammoType, spawnLocation, Quaternion.identity);
+
         Destroy(gameObject);
     }
 }
