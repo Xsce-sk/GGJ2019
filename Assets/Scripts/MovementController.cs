ffb5cbd321a928f3770184b223673cfe246f0626
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-01-25 23:34:41 -0800

    Updated Movement Controls

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,15 +5,58 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    private Rigidbody2D rb2d;
+    private float xVelocity = 0f;
+    private float yVelocity = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform
+        if (Input.GetKey(KeyCode.A))
+        {
+            xVelocity = -moveSpeed;            
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            xVelocity = moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            yVelocity = moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            yVelocity = -moveSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            xVelocity = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            xVelocity = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            yVelocity = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            yVelocity = 0;
+        }
+
+        rb2d.velocity = new Vector2(xVelocity, yVelocity);
     }
 }
