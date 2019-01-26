15557cbd4492b6e226a48bd830d5bb85bade7762
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-01-26 02:06:30 -0800

    Added Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    private TextMeshProUGUI time;
+
+    public float countdown = 300f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countdown -= Time.deltaTime;
+
+        int minutes = (int) (countdown % 3600 / 60);
+        int seconds = (int) (countdown % 3600 % 60);
+
+        if (seconds > 9)
+            time.text = minutes + ":" + seconds;
+        else
+            time.text = minutes + ":" + "0" + seconds;
     }
 }
