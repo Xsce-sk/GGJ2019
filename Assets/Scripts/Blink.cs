7cb6561a7840f56d1dfcb2b4ceac4bd54dc37f54
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-01-27 15:47:15 -0800

    Main Menu Updated

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink : MonoBehaviour
+{
+    public GameObject x;
+    public float counter = 1f;
+    private float count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = Time.time + counter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= count)
+        {
+            if (x.activeSelf)
+            {
+                x.SetActive(false);
+            }
+            else if(!x.activeSelf)
+                x.SetActive(true);
+
+            count = Time.time + counter;
+        }
+    }
+}
