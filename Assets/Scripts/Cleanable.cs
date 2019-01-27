f08a695cdf0d4fe9e54c850ae29e3a45c6545cd0
Ace <ace.lowder@gmail.com>
2019-01-26 18:24:47 -0800

    Created Cleaner and Cleanable Scripts

---
+++ Assets/Scripts/Cleanable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Cleanable : MonoBehaviour
+{
+    [Serializable]
+    public class CleanEvent : UnityEvent<Cleanable>
+    { }
+
+    public int startingDirtiness = 20;
+    public CleanEvent OnClean;
+    public CleanEvent OnFullyCleaned;
+
+    protected int m_CurrentDirtiness;
+
+    void Start()
+    {
+        m_CurrentDirtiness = startingDirtiness;
+    }
+
+    public void ReceiveClean()
+    {
+        m_CurrentDirtiness -= 1;
+        OnClean.Invoke(this);
+
+        if (m_CurrentDirtiness <= 0)
+        {
+            OnFullyCleaned.Invoke(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Cleaner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Cleaner : MonoBehaviour
+{
+    [Serializable]
+    public class CleanableEvent : UnityEvent<Cleaner, Cleanable>
+    { }
+
+    public KeyCode cleanKey;
+    public CleanableEvent OnClean;
+    
+    protected BoxCollider2D m_BoxCollider2D;
+    protected Collider2D m_Target;
+
+    protected bool m_CanClean;
+    protected bool m_IsCleaning;
+    
+    void Awake()
+    {
+        m_BoxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Dirty"))
+        {
+            m_CanClean = true;
+            m_Target = collision;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Dirty"))
+        {
+            m_CanClean = false;
+        }
+    }
+
+    void Update()
+    {
+        if (!m_IsCleaning)
+        {
+            if (Input.GetKey(cleanKey) && m_CanClean)
+            {
+                // DisablePlayerMovement()
+                // ChangeSprite()
+                m_IsCleaning = true;
+            }
+        }
+        else // Cleaning
+        {
+            if (Input.GetKey(cleanKey))
+            {
+                // EnablePlayerMovement()
+                // ResetSprite()
+                m_IsCleaning = false;
+            }
+
+            ReadCleaningInputs();
+        }
+    }
+
+    private void ReadCleaningInputs()
+    {
+        Cleanable cleanable = m_Target.GetComponent<Cleanable>();
+        OnClean.Invoke(this, cleanable);
+    }
+}
