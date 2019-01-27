67219ec9c1d204d76c4c2b6bb19bee674a1216c7
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-01-27 10:03:40 -0800

    Update ChildOrderLayerController.cs

--- Assets/Scripts/ChildOrderLayerController.cs
+++ Assets/Scripts/ChildOrderLayerController.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class ChildOrderLayerController : MonoBehaviour
 {
     private SpriteRenderer sr;
+    bool setOrder = true;
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        sr.sortingOrder = transform.parent.gameObject.GetComponent<SpriteRenderer>().sortingOrder + 1;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(setOrder)
+        {
+            sr.sortingOrder = transform.parent.gameObject.GetComponent<SpriteRenderer>().sortingOrder + 1;
+            setOrder = false;
+        }
     }
 }
