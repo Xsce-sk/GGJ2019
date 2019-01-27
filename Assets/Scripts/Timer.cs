7fd37a1875bd20a398ee73d9b7e40ea5a81fec3c
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-01-27 14:45:27 -0800

    Added UI to Main

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,6 +6,8 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     private TextMeshProUGUI time;
+    public GameObject UIPanel;
+    public GameObject EndScreenPanel;
 
     public float countdown = 300f;
 
@@ -18,14 +20,24 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        countdown -= Time.deltaTime;
+        if (countdown > 0)
+        {
+            countdown -= Time.deltaTime;
 
-        int minutes = (int) (countdown % 3600 / 60);
-        int seconds = (int) (countdown % 3600 % 60);
+            int minutes = (int)(countdown % 3600 / 60);
+            int seconds = (int)(countdown % 3600 % 60);
 
-        if (seconds > 9)
-            time.text = minutes + ":" + seconds;
-        else
-            time.text = minutes + ":" + "0" + seconds;
+            if (seconds > 9)
+                time.text = minutes + ":" + seconds;
+            else
+                time.text = minutes + ":" + "0" + seconds;
+        }
+
+        if(countdown <= 0)
+        {
+            time.text = "0:00";
+            UIPanel.SetActive(false);
+            EndScreenPanel.SetActive(true);
+        }
     }
 }
