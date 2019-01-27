0cb62ad15b76f3d53ce5cb84d53182aecbb04576
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-01-26 20:35:22 -0800

    Updated Load Scene

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject credits;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +23,24 @@ public class LoadScene : MonoBehaviour
     public void LoadSceneIndex(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenCredits()
+    {
+        if(menu.activeSelf)
+        {
+            menu.SetActive(false);
+            credits.SetActive(true);
+        }
+        else if(credits.activeSelf)
+        {
+            menu.SetActive(true);
+            credits.SetActive(false);
+        }
     }
 }
